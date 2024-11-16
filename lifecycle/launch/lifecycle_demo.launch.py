f088463e31edde662bfb2f998817b61919f852ee
Nick Laurenson <nick.laurenson.ext@exxact-robotics.com>
2024-11-16 14:07:00 +0100

    reproduce bug of vscode ros2 extension

--- lifecycle/launch/lifecycle_demo.launch.py
+++ lifecycle/launch/lifecycle_demo.launch.py
@@ -16,13 +16,37 @@ from launch import LaunchDescription
 from launch.actions import Shutdown
 from launch_ros.actions import LifecycleNode
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler, LogInfo
+from launch_ros.event_handlers import OnStateTransition
+
+from launch_ros.events.lifecycle import ChangeState
+import launch
+from lifecycle_msgs.msg import Transition
 
 
 def generate_launch_description():
+    node = LifecycleNode(package='lifecycle', executable='lifecycle_talker',
+                         name='lc_talker', namespace='', output='screen')
+
+    configure_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=launch.events.matches_action(
+                node),
+            transition_id=Transition.TRANSITION_CONFIGURE,
+        ))
+
+    state_handler = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=node,
+            goal_state='inactive',
+            entities=[
+                # Log
+                LogInfo(
+                    msg="RegisterEventHandler worked")]))
+
     return LaunchDescription([
-        LifecycleNode(package='lifecycle', executable='lifecycle_talker',
-                      name='lc_talker', namespace='', output='screen'),
-        Node(package='lifecycle', executable='lifecycle_listener', output='screen'),
-        Node(package='lifecycle', executable='lifecycle_service_client', output='screen',
-             on_exit=Shutdown()),
+        configure_event, node, state_handler
+        # Node(package='lifecycle', executable='lifecycle_listener', output='screen'),
+        # Node(package='lifecycle', executable='lifecycle_service_client', output='screen',
+        #      on_exit=Shutdown()),
     ])
